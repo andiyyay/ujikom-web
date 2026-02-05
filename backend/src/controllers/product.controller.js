2e5bec60a4354dcf5970b5439b2e4aea90926084
andiyyay <ndyyaxzydbca@gmail.com>
2026-02-05 07:39:07 +0700

    hapus backend folder dari frontend branch

--- backend/src/controllers/product.controller.js
+++
@@ -1,8 +0,0 @@
-const db = require("../config/db");
-
-exports.getProducts = (req, res) => {
-  db.query("SELECT * FROM products", (err, results) => {
-    if (err) return res.status(500).json(err);
-    res.json(results);
-  });
-};
