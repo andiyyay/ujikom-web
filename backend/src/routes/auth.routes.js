650cde6e93564882ee1ddbe73c054e400c205196
andiyyay <ndyyaxzydbca@gmail.com>
2026-02-13 10:54:28 +0700

    edited

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -4,7 +4,19 @@ const router = express.Router();
 router.post("/login", (req, res) => {
   const { email, password } = req.body;
 
-  if (email === "admin@gmail.com" && password === "123456") {
+  if (email === "test@gmail.com" && password === "123456") {
+    return res.json({
+      success: true,
+      message: "Login berhasil",
+      user: {
+        id: 1,
+        email,
+      },
+      token: "dummy-token",
+    });
+  }
+
+    if (email === "a@gmail.com" && password === "a") {
     return res.json({
       success: true,
       message: "Login berhasil",
@@ -20,6 +32,6 @@ router.post("/login", (req, res) => {
     success: false,
     message: "Email atau password salah",
   });
-});
+});1
 
 module.exports = router;
